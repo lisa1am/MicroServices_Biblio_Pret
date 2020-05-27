a1dde3e9f42d18c5a4ab53ed9e1bb35f8de9d2a2
Samuel <sam.arzur@gmail.com>
2020-05-27 18:31:14 +0200

    Mise à jour PretRepository

--- src/main/java/fr/dauphine/miageif/msa/Biblio/Repository/PretRepository.java
+++ src/main/java/fr/dauphine/miageif/msa/Biblio/Repository/PretRepository.java
@@ -3,17 +3,22 @@ package fr.dauphine.miageif.msa.Biblio.Repository;
 import fr.dauphine.miageif.msa.Biblio.Pret;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Date;
 import java.util.List;
 
 public interface PretRepository extends JpaRepository<Pret, Long> {
-    /*Pret findByIsbn(String isbn);
 
-    List<Pret> findAllByAuteur(String Auteur);
+    List<Pret> findAllByIdLecteur(Long IdLecteur);
 
-    void deleteByIsbn(String isbn);
+    List<Pret> findAllByIsbn(String isbn);
 
-    boolean existsByIsbn(String isbn);*/
+    Pret findAllByIdLecteurAndIsbn(Long IdLecteur, String isbn);
 
+    List<Pret> findAllByDate(Date date_emprunt);
 
+    List<Pret> findAll();
 
+    boolean existsByIsbn(String isbn);
+
+    boolean existsByIdLecteur(Long id);
 }
